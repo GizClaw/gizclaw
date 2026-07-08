4eaa7209d3f187268f946d8e46b20073b1735fff
idy <idy0013@gmail.com>
2026-07-09 03:23:54 +0800

    rpc: source payload maps from peer proto

--- pkgs/gizclaw/api/rpcapi/codegen.go
+++ pkgs/gizclaw/api/rpcapi/codegen.go
@@ -2,3 +2,4 @@ package rpcapi
 
 //go:generate go run ./internal/rpcgen -resolve
 //go:generate go tool oapi-codegen -config=codegen_config.yaml -o generated.go rpc_resolved.json
+//go:generate go run ../../../../tools/gzc-rpcproto-gen -proto ../../../../api/rpc/peer.proto -go-rpcapi-out payload_proto_gen.go
--- pkgs/gizclaw/api/rpcproto/codegen.go
+++ pkgs/gizclaw/api/rpcproto/codegen.go
@@ -1,4 +1,3 @@
 package rpcpb
 
-//go:generate go run ../../../../tools/gzc-rpcproto-gen -schema ../rpcapi/rpc_resolved.json -out ../../../../api/rpc/payload.proto -go-rpcapi-out ../rpcapi/payload_proto_gen.go
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --proto_path=../../../../api/rpc ../../../../api/rpc/common.proto ../../../../api/rpc/peer.proto ../../../../api/rpc/payload.proto
