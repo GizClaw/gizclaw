cbca89012683251c8678d6da60c2d2af78456ebd
idy <idy0013@gmail.com>
2026-07-12 09:44:38 +0800

    apps: address Pixa review feedback

--- apps/gizclaw-app/lib/main.dart
+++ apps/gizclaw-app/lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:gizclaw/gizclaw.dart';
+
+import 'pixa_sprite.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +13,136 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GizClaw Pixa',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xff0f8b8d),
+          brightness: Brightness.light,
+        ),
+        scaffoldBackgroundColor: const Color(0xfff7f9fb),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PixaSmokePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PixaSmokePage extends StatelessWidget {
+  const PixaSmokePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  static final PixaAsset _asset = validatePixa(
+    _makePixaSmokeAsset(),
+    mode: PixaValidationMode.petdef,
+  );
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  static const Duration _previewTime = Duration(milliseconds: 180);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('GizClaw Pixa'),
+        backgroundColor: const Color(0xfff7f9fb),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
+        minimum: const EdgeInsets.all(24),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Text('You have pushed the button this many times:'),
+            Text('Miso Preview', style: textTheme.headlineSmall),
+            const SizedBox(height: 24),
+            Expanded(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xffb8c2cc)),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Center(
+                  child: PixaSprite(
+                    asset: _asset,
+                    clipName: 'idle',
+                    elapsed: _previewTime,
+                    width: 192,
+                    height: 96,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'clip idle  |  ${_asset.canvas.width}x${_asset.canvas.height}  |  ${_asset.frames.length} frames',
+              style: textTheme.bodyMedium?.copyWith(
+                color: const Color(0xff0f766e),
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+}
+
+Uint8List _makePixaSmokeAsset() {
+  const headerSize = 40;
+  const clipEntrySize = 56;
+  const frameEntrySize = 16;
+  const width = 2;
+  const height = 1;
+  const framePayloads = [
+    [0x00, 0xf8, 0x1f, 0x00],
+    [0x1f, 0x00, 0xe0, 0x07],
+  ];
+  const frameDurations = [140, 260];
+  const paletteOffset = headerSize;
+  const clipOffset = paletteOffset + 2;
+  const frameOffset = clipOffset + clipEntrySize;
+  final payloadOffset = frameOffset + frameEntrySize * framePayloads.length;
+  final payloadSize = framePayloads.fold<int>(
+    0,
+    (total, payload) => total + payload.length,
+  );
+  final bytes = Uint8List(payloadOffset + payloadSize);
+  final data = ByteData.sublistView(bytes);
+
+  bytes.setAll(0, ascii.encode('PIXA'));
+  data.setUint16(4, 1, Endian.little);
+  data.setUint16(6, headerSize, Endian.little);
+  data.setUint16(8, width, Endian.little);
+  data.setUint16(10, height, Endian.little);
+  data.setUint16(12, 1, Endian.little);
+  data.setUint16(14, 1, Endian.little);
+  data.setUint32(16, framePayloads.length, Endian.little);
+  data.setUint32(20, paletteOffset, Endian.little);
+  data.setUint32(24, clipOffset, Endian.little);
+  data.setUint32(28, frameOffset, Endian.little);
+  data.setUint32(32, payloadOffset, Endian.little);
+  data.setUint32(36, payloadSize, Endian.little);
+
+  bytes.setAll(clipOffset, utf8.encode('idle'));
+  data.setUint32(clipOffset + 40, 1, Endian.little);
+  data.setUint32(clipOffset + 44, 400, Endian.little);
+  data.setUint16(clipOffset + 48, framePayloads.length, Endian.little);
+
+  var payloadCursor = payloadOffset;
+  for (var index = 0; index < framePayloads.length; index += 1) {
+    final entryOffset = frameOffset + frameEntrySize * index;
+    final payload = framePayloads[index];
+    data.setUint16(entryOffset, frameDurations[index], Endian.little);
+    data.setUint32(
+      entryOffset + 4,
+      payloadCursor - payloadOffset,
+      Endian.little,
+    );
+    data.setUint32(entryOffset + 8, payload.length, Endian.little);
+    bytes.setAll(payloadCursor, payload);
+    payloadCursor += payload.length;
+  }
+
+  return bytes;
 }
